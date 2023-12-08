a2dfb31dee8326ec775882ca999764bddfb99ac7
nrfabrlianti <145241751+nrfabrlianti@users.noreply.github.com>
2023-12-08 09:45:57 +0700

    Add files via upload

---
+++ PERTEMUAN10/variabel.php
@@ -0,0 +1,15 @@
+<?php
+
+$a ="hello";
+$hello = "Hello World!";
+
+//menampilkan isi variabel $a
+//hello 
+echo $a . "</br>";
+
+//menampilkan isi dari variabel dengan nama yang sama seperti isi variabel $a
+// isi variabel $a = hello. jadi, nanti akan menampilkan isi dari variabel $hello
+//Hello World
+echo $$a . "</br>";
+
+?>
---
+++ PERTEMUAN10/variabel_operator.php
@@ -0,0 +1,18 @@
+<?php
+
+// menetapkan nilai
+$x = 21;
+$y = 3;
+
+echo "x = $x</br>";
+echo "y = $y</br>";
+
+//pembagian
+$hasilBagi = $x / $y; // tanda / adalah operator
+echo "hasil bagi dari x / y = $hasilBagi </br>";
+
+//perkalian
+$hasilKali = $x * $y; //tanda * adalah operator
+echo "hasil bagi dari x * y = $hasilKali </br>";
+
+?>
